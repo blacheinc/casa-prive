36ec3308488a129c4d49fd0bb5e3e87ac5e89df8
abbott-m <kampadu123@gmail.com>
2025-10-17 11:45:21 +0000

    Update

--- src/app/admin/menu-items/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MenuItemsPage() {
-  return (
-    <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold">Menu Items</h1>
-      <p>Manage your menu items here.</p>
-    </div>
-  );
-}
